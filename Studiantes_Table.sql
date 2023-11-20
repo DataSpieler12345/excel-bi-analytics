4c129db933e62b4124ce80511da5766ba90da011
BI - Data Analyst <45371372+DataSpieler12345@users.noreply.github.com>
2023-11-20 22:02:41 +0100

    commit data-project

---
+++ Studiantes_Table.sql
@@ -0,0 +1,30 @@
+USE school_db;
+
+create table  Studiantes2
+(
+	id int auto_increment primary key,
+	last_name nvarchar(50) not null,
+	first_name nvarchar(50) not null,
+	date_of_birth date not null,
+	enrollment_date date 
+);
+
+-- La cantidad de campos va ser igual a la cantidad de los valores
+INSERT INTO studiantes 
+(last_name, first_name, date_of_birth, enrollment_date)
+values
+('Test', 'Student', '2001-09-01', null);
+
+-- INSERT INTO TableName(Columns...) VALUES (values....), (values....), (values....)
+-- Multiple Row Insert
+INSERT INTO studiantes 
+(last_name, first_name, date_of_birth, enrollment_date)
+values
+('Test1', 'Student1', '2001-01-01', '2019-03-01'),
+('Test2', 'Student2', '2001-02-01', null),
+('Test3', 'Student3', '2001-03-01', '2019-03-01'),
+('Test4', 'Student4', '2001-04-01', null),
+('Test5', 'Student5', '2001-05-01', '2019-03-01'),
+('Test6', 'Student6', '2001-06-01', null),
+('Test7', 'Student7', '2001-07-01', null),
+('Test8', 'Student8', '2001-08-01', null);
---
+++ selecting_data.sql
@@ -0,0 +1,6 @@
+SELECT 
+last_name as 'Last Name',
+first_name 'First Name',
+date_of_birth 'Date of Birth'
+FROM studiantes;
+
